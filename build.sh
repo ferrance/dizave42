e1108c11bb7d85f2cf86db2514ed2d041c1dc2a5
ferrance <dave@ferrance.com>
2024-01-02 18:54:19 -0700

    clean up build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 qmk clean
 qmk compile -e CONVERT_TO=kb2040 -e BIG_OLED=yes
-mv ../../../../crkbd_rev1_dizave42_kb2040.uf2 ../../../../crkbd_dizave42_bigoled.uf2
-cp ../../../../crkbd_rev1_dizave42_kb2040.uf2 .
+mv ~/qmk_firmware/crkbd_rev1_dizave42_kb2040.uf2 ~/qmk_firmware/crkbd_dizave42_bigoled.uf2
+cp ~/qmk_firmware/crkbd_dizave42_bigoled.uf2 .
 qmk compile -e CONVERT_TO=kb2040 
-mv ../../../../crkbd_rev1_dizave42_kb2040.uf2 ../../../../crkbd_dizave42.uf2
-cp ../../../../crkbd_rev1_dizave42_kb2040.uf2 .
+mv ~/qmk_firmware/crkbd_rev1_dizave42_kb2040.uf2 ~/qmk_firmware/crkbd_dizave42.uf2
+cp ~/qmk_firmware/crkbd_dizave42.uf2 .
