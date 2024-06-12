83718bed6d9a3679d0f250283ffcf2ea621e9c08
ferrance <dave@ferrance.com>
2024-06-12 09:13:59 -0600

    add support for elitec to build process

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ cp ~/qmk_firmware/crkbd_dizave42_bigoled.uf2 ./firmware
 qmk compile -c 
 mv ~/qmk_firmware/crkbd_rev1_dizave42_kb2040.uf2 ~/qmk_firmware/crkbd_dizave42.uf2
 cp ~/qmk_firmware/crkbd_dizave42.uf2 ./firmware
+qmk compile -c -e CONVERT_TO=promicro_rp2040
+mv ~/qmk_firmware/crkbd_rev1_dizave42_promicro_rp2040.uf2 ~/qmk_firmware/crkbd_dizave42_elitepi.uf2
+cp ~/qmk_firmware/crkbd_dizave42_elitepi.uf2 ./firmware
