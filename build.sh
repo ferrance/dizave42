f78ebbccf530731eb6796ccd2b8360679a1a3b18
ferrance <dave@ferrance.com>
2025-07-04 12:47:35 -0600

    fix build.sh to use a non-deprecated converter

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ mv ~/qmk_firmware/crkbd_rev1_dizave42.uf2 ~/qmk_firmware/crkbd_dizave42_rp2040.u
 cp ~/qmk_firmware/crkbd_dizave42_rp2040.uf2 ./firmware
 
 # this is for the other pinout that is like $2 on 
-# the promicro is deprecated, I'll need to figure out to switch to either sparkfun_pm2040 or rp2040_ce
-qmk compile -c -e CONVERT_TO=promicro_rp2040
+qmk compile -c -e CONVERT_TO=elite_pi
 mv ~/qmk_firmware/crkbd_rev1_dizave42_promicro_rp2040.uf2 ~/qmk_firmware/crkbd_dizave42_elitepi.uf2
 cp ~/qmk_firmware/crkbd_dizave42_elitepi.uf2 ./firmware
